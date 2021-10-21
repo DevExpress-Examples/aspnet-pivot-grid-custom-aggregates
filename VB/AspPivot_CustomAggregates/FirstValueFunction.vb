fb6f2584cef910ba8d1733cbbc37c1226a4508ae
ritazakhodyaeva <57389482+ritazakhodyaeva@users.noreply.github.com>
2021-10-21 22:49:48 +0300

    Update FirstValueFunction.vb

--- VB/AspPivot_CustomAggregates/FirstValueFunction.vb
+++ VB/AspPivot_CustomAggregates/FirstValueFunction.vb
@@ -3,59 +3,45 @@ Imports DevExpress.DataProcessing.Criteria
 Imports System
 
 Namespace Dashboard_FirstValueAggregate
-
     Friend Class FirstValueAggregateFunction
         Implements ICustomAggregateFunction, ICustomFunctionOperatorBrowsable
-
         Public ReadOnly Property Name As String Implements ICustomFunctionOperator.Name
             Get
                 Return "FirstValue"
             End Get
         End Property
-
         Public ReadOnly Property MinOperandCount As Integer Implements ICustomFunctionOperatorBrowsable.MinOperandCount
             Get
                 Return 1
             End Get
         End Property
-
         Public ReadOnly Property MaxOperandCount As Integer Implements ICustomFunctionOperatorBrowsable.MaxOperandCount
             Get
                 Return 1
             End Get
         End Property
-
         Public ReadOnly Property Description As String Implements ICustomFunctionOperatorBrowsable.Description
             Get
                 Return "Aggregates data by input value, and displays the first value of the field"
             End Get
         End Property
-
         Public ReadOnly Property Category As FunctionCategory Implements ICustomFunctionOperatorBrowsable.Category
             Get
                 Return FunctionCategory.Text
             End Get
         End Property
-
         Public Function Evaluate(ParamArray operands As Object()) As Object Implements ICustomFunctionOperator.Evaluate
             Throw New NotImplementedException()
         End Function
-
-        'public string Format(Type providerType, params string[] operands) {
-        '    return string.Format("FIRST_VALUE({0})", operands[0]);
-        '}
         Public Function GetAggregationContextType(ByVal inputType As Type) As Type Implements ICustomAggregateFunction.GetAggregationContextType
             Return GetType(FirstValueAggregateState(Of )).MakeGenericType(inputType)
         End Function
-
         Public Function IsValidOperandCount(ByVal count As Integer) As Boolean Implements ICustomFunctionOperatorBrowsable.IsValidOperandCount
             Return count <= MaxOperandCount AndAlso count >= MinOperandCount
         End Function
-
         Public Function IsValidOperandType(ByVal operandIndex As Integer, ByVal operandCount As Integer, ByVal type As Type) As Boolean Implements ICustomFunctionOperatorBrowsable.IsValidOperandType
             Return IsValidOperandCount(operandCount) AndAlso operandIndex = 0
         End Function
-
         Public Function ResultType(ParamArray operands As Type()) As Type Implements ICustomFunctionOperator.ResultType
             Return operands(0)
         End Function
@@ -63,15 +49,11 @@ Namespace Dashboard_FirstValueAggregate
 
     Friend Class FirstValueAggregateState(Of TInput)
         Implements ICustomAggregateFunctionContext(Of TInput, TInput)
-
         Private isSet As Boolean = False
-
         Private firstValue As TInput
-
         Public Function GetResult() As TInput Implements ICustomAggregateFunctionContext(Of TInput, TInput).GetResult
             Return If(isSet, firstValue, Nothing)
         End Function
-
         Public Sub Process(ByVal value As TInput) Implements ICustomAggregateFunctionContext(Of TInput, TInput).Process
             If Not isSet Then
                 firstValue = value
