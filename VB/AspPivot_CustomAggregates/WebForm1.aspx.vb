caaff9bf5d9599b73511b38b9b2c5820a06827b2
DevExpressExampleBot <fakeEmail>
2021-10-22 16:37:33 +0300

    Source auto update [skip ci]

--- VB/AspPivot_CustomAggregates/WebForm1.aspx.vb
+++ VB/AspPivot_CustomAggregates/WebForm1.aspx.vb
@@ -3,10 +3,8 @@ Imports DevExpress.Data.Filtering
 Imports System
 
 Namespace AspPivot_CustomFunctions
-
     Public Partial Class WebForm1
         Inherits Web.UI.Page
-
         Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
             Call CriteriaOperator.RegisterCustomFunction(New FirstValueAggregateFunction())
         End Sub
