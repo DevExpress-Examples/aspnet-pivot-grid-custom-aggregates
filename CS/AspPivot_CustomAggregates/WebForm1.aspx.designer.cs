1e555c3d4357978df8bfae9b6c59a01bbf45e9f0
Natalia Kazakova <natalia.kazakova@devexpress.com>
2022-09-29 11:34:48 +0400

    fix build

--- CS/AspPivot_CustomAggregates/WebForm1.aspx.designer.cs
+++ CS/AspPivot_CustomAggregates/WebForm1.aspx.designer.cs
@@ -8,10 +8,10 @@
 //------------------------------------------------------------------------------
 
 namespace AspPivot_CustomFunctions {
-    
-    
+
+
     public partial class WebForm1 {
-        
+
         /// <summary>
         /// form1 control.
         /// </summary>
@@ -20,7 +20,7 @@ namespace AspPivot_CustomFunctions {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-        
+
         /// <summary>
         /// ASPxPivotGrid1 control.
         /// </summary>
@@ -29,7 +29,7 @@ namespace AspPivot_CustomFunctions {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::DevExpress.Web.ASPxPivotGrid.ASPxPivotGrid ASPxPivotGrid1;
-        
+
         /// <summary>
         /// fieldCountry control.
         /// </summary>
@@ -38,7 +38,7 @@ namespace AspPivot_CustomFunctions {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::DevExpress.Web.ASPxPivotGrid.PivotGridField fieldCountry;
-        
+
         /// <summary>
         /// fieldProductName control.
         /// </summary>
@@ -47,7 +47,7 @@ namespace AspPivot_CustomFunctions {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::DevExpress.Web.ASPxPivotGrid.PivotGridField fieldProductName;
-        
+
         /// <summary>
         /// fieldCategoryName control.
         /// </summary>
@@ -56,7 +56,7 @@ namespace AspPivot_CustomFunctions {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::DevExpress.Web.ASPxPivotGrid.PivotGridField fieldCategoryName;
-        
+
         /// <summary>
         /// fieldExtendedPrice control.
         /// </summary>
@@ -65,7 +65,7 @@ namespace AspPivot_CustomFunctions {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::DevExpress.Web.ASPxPivotGrid.PivotGridField fieldExtendedPrice;
-        
+
         /// <summary>
         /// fieldSalesPerson control.
         /// </summary>
@@ -74,7 +74,7 @@ namespace AspPivot_CustomFunctions {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::DevExpress.Web.ASPxPivotGrid.PivotGridField fieldSalesPerson;
-        
+
         /// <summary>
         /// FirstSoldValue control.
         /// </summary>
@@ -83,7 +83,16 @@ namespace AspPivot_CustomFunctions {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::DevExpress.Web.ASPxPivotGrid.PivotGridField FirstSoldValue;
-        
+
+        /// <summary>
+        /// SqlDataSource2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource2;
+
         /// <summary>
         /// SqlDataSource1 control.
         /// </summary>
