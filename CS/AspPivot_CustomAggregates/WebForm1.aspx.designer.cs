585d84644a57c9594376c799c35d0c0e182a8daa
Natalia Kazakova <natalia.kazakova@devexpress.com>
2022-09-29 12:21:35 +0400

    fix file inclusion

--- CS/AspPivot_CustomAggregates/WebForm1.aspx.designer.cs
+++ CS/AspPivot_CustomAggregates/WebForm1.aspx.designer.cs
@@ -85,15 +85,6 @@ namespace AspPivot_CustomFunctions {
         protected global::DevExpress.Web.ASPxPivotGrid.PivotGridField FirstSoldValue;
 
         /// <summary>
-        /// SqlDataSource2 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource2;
-
-        /// <summary>
         /// SqlDataSource1 control.
         /// </summary>
         /// <remarks>
